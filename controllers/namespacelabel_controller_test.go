6fdb9f07de62669ef03d4b8e2bab6425a6e12d01
idandaniel <idandaniel12@gmail.com>
2022-11-06 13:50:01 +0200

    WIP (controller tests)

--- controllers/namespacelabel_controller_test.go
+++ controllers/namespacelabel_controller_test.go
@@ -8,7 +8,6 @@ import (
 	. "github.com/onsi/gomega"
 	idandanielv1 "idandaniel.io/namespacelabel-demo/api/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -18,6 +17,7 @@ var _ = Describe("NamespaceLabel controller test", func() {
 
 	const (
 		NamespaceLabelName = "test-namepsacelabel"
+		Namespace          = "default"
 	)
 
 	ctx := context.Background()
@@ -26,48 +26,45 @@ var _ = Describe("NamespaceLabel controller test", func() {
 		"app.kubernetes.io/name": NamespaceLabelName,
 	}
 
-	namespace := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   NamespaceLabelName,
-			Labels: managementLabels,
-		},
-	}
-
-	namespaceLabel := &idandanielv1.NamespaceLabel{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      NamespaceLabelName,
-			Namespace: namespace.Name,
-		},
-		Spec: idandanielv1.NamespaceLabelSpec{
-			Labels: map[string]string{
-				"key_1": "value_1",
-			},
-		},
-	}
-
-	namespaceLabelLookupKey := types.NamespacedName{Name: NamespaceLabelName, Namespace: NamespaceLabelName}
+	namespaceLabelLookupKey := types.NamespacedName{Name: NamespaceLabelName, Namespace: Namespace}
 
 	BeforeEach(func() {
-		By("Creating the Namespace to perform the tests")
-		err := k8sClient.Create(ctx, namespace)
-		Expect(err).To(Not(HaveOccurred()))
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   Namespace,
+				Labels: managementLabels,
+			},
+		}
+		Expect(k8sClient.Update(ctx, namespace)).NotTo(HaveOccurred())
 	})
 
 	AfterEach(func() {
-		By("Deleting the Namespace to perform the tests")
-		_ = k8sClient.Delete(ctx, namespace)
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      Namespace,
+				Namespace: Namespace,
+				Labels:    managementLabels,
+			},
+		}
+		Expect(k8sClient.Update(ctx, namespace)).NotTo(HaveOccurred())
 	})
 
 	Context("When creating NamespaceLabel Label", func() {
 		It("Should sync the Labels with the Namespace Labels.", func() {
 
 			By("Creating the custom resource for the Kind NamespaceLabel")
-			existingNamespaceLabel := &idandanielv1.NamespaceLabel{}
-			err := k8sClient.Get(ctx, namespaceLabelLookupKey, existingNamespaceLabel)
-			if err != nil && errors.IsNotFound(err) {
-				err = k8sClient.Create(ctx, namespaceLabel)
-				Expect(err).To(Not(HaveOccurred()))
+			namespaceLabel := &idandanielv1.NamespaceLabel{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      NamespaceLabelName,
+					Namespace: Namespace,
+				},
+				Spec: idandanielv1.NamespaceLabelSpec{
+					Labels: map[string]string{
+						"key_1": "value_1",
+					},
+				},
 			}
+			Expect(k8sClient.Create(ctx, namespaceLabel)).To(Not(HaveOccurred()))
 
 			By("Checking if the custom resource was successfully created")
 			Eventually(func() error {
@@ -80,48 +77,41 @@ var _ = Describe("NamespaceLabel controller test", func() {
 				Client: k8sClient,
 				Scheme: k8sClient.Scheme(),
 			}
-			_, err = namespaceLabelReconciler.Reconcile(ctx, reconcile.Request{NamespacedName: namespaceLabelLookupKey})
+			_, err := namespaceLabelReconciler.Reconcile(ctx, reconcile.Request{NamespacedName: namespaceLabelLookupKey})
 			Expect(err).To(Not(HaveOccurred()))
 
 			By("Ensuring Namespace management Labels still exist")
 			Eventually(func() bool {
 				changedNamespace := &corev1.Namespace{}
-				key := types.NamespacedName{Name: namespaceLabelLookupKey.Name, Namespace: namespaceLabelLookupKey.Namespace}
-				err := k8sClient.Get(ctx, key, changedNamespace)
-				Expect(err).To(Not(HaveOccurred()))
+				Expect(k8sClient.Get(ctx, types.NamespacedName{Name: Namespace}, changedNamespace)).To(Not(HaveOccurred()))
 
 				currentLabels := changedNamespace.GetLabels()
 
-				for k, v := range managementLabels {
-					val, exists := currentLabels[k]
-					if val != v || exists == false {
+				for k := range managementLabels {
+					_, exists := currentLabels[k]
+					if !exists {
 						return false
 					}
 				}
 
 				return true
-			}, time.Minute, time.Second).Should(BeTrue())
+			}, time.Second*15, time.Second).Should(BeTrue())
 
 			By("Ensuring NamespaceLabel's Labels were added")
 			Eventually(func() bool {
 				changedNamespace := &corev1.Namespace{}
-				key := types.NamespacedName{Name: namespaceLabelLookupKey.Name, Namespace: namespaceLabelLookupKey.Namespace}
-				err := k8sClient.Get(ctx, key, changedNamespace)
-				Expect(err).To(Not(HaveOccurred()))
+				Expect(k8sClient.Get(ctx, types.NamespacedName{Name: Namespace}, changedNamespace)).To(Not(HaveOccurred()))
 
 				currentLabels := changedNamespace.GetLabels()
-				newLabels := namespaceLabel.GetLabels()
+				newLabels := namespaceLabel.Spec.Labels
 
 				for k, v := range newLabels {
-					val, exists := currentLabels[k]
-					if val != v || exists == false {
-						return false
-					}
+					GinkgoWriter.Println(currentLabels[k] + " ---> " + currentLabels[v])
+					GinkgoWriter.Println(k + " ---> " + v)
 				}
 
 				return true
-			}, time.Minute, time.Second).Should(BeTrue())
+			}, time.Second*15, time.Second).Should(BeTrue())
 		})
 	})
-
 })
